d006a6d42b4dba2394811eafb42dd814615fc1dc
Luis Heredia <heredialuis20@gmail.com>
2023-10-22 18:06:06 +0200

    to be or not to be

---
+++ toBeOrNotToBe.js
@@ -0,0 +1,58 @@
+function expect(val) {
+
+    return {
+        toBe: function (toBeVal) {
+
+            if (val === toBeVal) {
+
+                return true;
+
+            } else {
+
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function (notToBeVal) {
+
+            if (notToBeVal !== val) {
+
+                return true;
+
+            } else {
+
+                throw Error("Equal");
+
+            }
+        }
+    };
+}
+
+
+
+try {
+
+    console.log(expect(5).toBe(5));
+
+} catch (error) {
+
+    console.error(error.message);
+}
+
+try {
+
+    console.log(expect(5).toBe(null));
+
+} catch (error) {
+
+    console.error(error.message);
+}
+
+try {
+
+    console.log(expect(5).notToBe(null));
+
+} catch (error) {
+
+    console.error(error.message);
+}
